cce33d5ac6721518fe058831e48c9630df450a74
Kevin Malley <kevin.malley@outlook.com>
2023-10-19 00:17:57 +0100

    Basically working

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -41,18 +41,20 @@ rules = {
 def resetGame():
     rockBtn["state"] = "active"
     paperBtn["state"] = "active"
-    scissorsBtn["state"] = "active"
+    scissorBtn["state"] = "active"
     playerScoreLbl.config(text="Player:")
-    CpuScoreLbl.config(text="CPU")
+    cpuScoreLbl.config(text="CPU:")
+    cpuChoiceLbl.config(text="")
     roundResultLbl.config(text="")
 
 def disableBtns():
     rockBtn["state"] = "disable"
     paperBtn["state"] = "disable"
-    scissorsBtn["state"] = "disable"
+    scissorBtn["state"] = "disable"
 
-def playersMove(int playerChoice):
+def playersMove(playerChoice):
     cpuChoice = random.randint(0, 2)
+    cpuChoiceLbl.config(text=cpuChoice)
     roundResultInt = rules[playerChoice][cpuChoice]
     if roundResultInt == 1:
         roundResult = "Player wins"
@@ -62,4 +64,50 @@ def playersMove(int playerChoice):
         roundResult = "Draw"
     roundResultLbl.config(text=roundResult)
 
-    
+# Labels, Frames & Buttons
+tk.Label(window,
+      text="Rock Paper Scissor",
+      font="normal 20 bold",
+      fg="blue").pack(pady=20)     
+
+frame = tk.Frame(window)
+frame.pack()
+
+playerScoreLbl = tk.Label(frame,
+                       text="Player Score:",
+                       font=10)
+cpuScoreLbl = tk.Label(frame,
+                       text="CPU Score:",
+                       font=10)
+cpuChoiceLbl = tk.Label(frame,
+                        text="CPU pick",
+                        font=10)
+playerScoreLbl.pack(side=tk.LEFT)
+cpuScoreLbl.pack(side=tk.LEFT)
+cpuChoiceLbl.pack(side=tk.RIGHT)
+roundResultLbl = tk.Label(window,
+                       text="",
+                       font="normal 20 bold",
+                       bg="white",
+                       width=15,
+                       borderwidth=2,
+                       relief="solid")
+roundResultLbl.pack(pady=20)
+frame = tk.Frame(window)
+frame.pack()
+
+rockBtn = tk.Button(frame, text="Rock",
+                 font=10, width=7,
+                 command=lambda: playersMove(0))
+paperBtn = tk.Button(frame, text="Paper",
+                 font=10, width=7,
+                 command=lambda: playersMove(1))
+scissorBtn = tk.Button(frame, text="Scissor",
+                 font=10, width=7,
+                 command=lambda: playersMove(2))
+rockBtn.pack(side=tk.LEFT, padx=10)
+paperBtn.pack(side=tk.LEFT, padx=10)
+scissorBtn.pack(padx=10)
+
+# MAIN
+window.mainloop()
